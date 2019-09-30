b1b30860f5b46de05ecd0767a93534379d7f628e
Luis Solier Sajami <luis.sajami@gmail.com>
2019-09-30 00:45:12 -0500

    adding validaction to model

--- src/main/java/com/lsolier/udacity/chatroom/controller/WebSocketChatServer.java
+++ src/main/java/com/lsolier/udacity/chatroom/controller/WebSocketChatServer.java
@@ -5,10 +5,27 @@ import com.lsolier.udacity.chatroom.model.User;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class WebSocketChatServer {
+
+  @RequestMapping("")
+  public String redirectToLogin(User user) {
+    return "login";
+  }
+
+  @RequestMapping("/singIn")
+  public String redirectToChatRoom(@Valid User user, BindingResult result) {
+    if (result.hasErrors()) {
+      return "login";
+    }
+    System.out.println("User " + user.getName() + " login");
+    return "chat";
+  }
 
   @MessageMapping("/chat")
   @SendTo("/topic/chat")
@@ -16,8 +33,4 @@ public class WebSocketChatServer {
     return new Message(user.getName() + ": " + message);
   }
 
-  @RequestMapping("/login")
-  public String redirectToLogin() {
-    return "login";
-  }
 }
--- src/main/java/com/lsolier/udacity/chatroom/model/User.java
+++ src/main/java/com/lsolier/udacity/chatroom/model/User.java
@@ -1,11 +1,18 @@
 package com.lsolier.udacity.chatroom.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
+  @NotBlank(message = "User name can not be empty")
   private String name;
 }
